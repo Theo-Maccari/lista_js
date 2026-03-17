05ca30b57c76b2881807954973e6c22240a34c10
theo-maccari <theo_maccari@estudante.sesisenai.org.br>
2026-03-17 08:47:09 -0300

    add - pirmeiro exercício finalizado

---
+++ exer_01.js
@@ -0,0 +1,22 @@
+
+function somarNum() {
+    let soma = 0;
+    let continuar = true;
+
+    while (continuar) {
+
+        let numero = parseFloat(prompt("Digite um número (ou digite '0' para encerrar):"));
+
+        if(isNaN(numero)){
+            alert("Por favor, digite um número válido");
+        }else{
+            soma += numero;
+        }
+
+        continuar = confirm("Deseja adicionar mais números?");
+
+    };
+
+}
+
+somarNum();
