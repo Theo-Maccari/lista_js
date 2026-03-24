0993c1b8570655489b89a77f255f05b1b45e8351
theo-maccari <theo_maccari@estudante.sesisenai.org.br>
2026-03-24 08:59:54 -0300

    add - exercício 5 e 6 finalizados

--- exer_05.js
+++ exer_05.js
@@ -1,9 +1,13 @@
 function trasformação() {
 
-let num = []
+    let num = []
 
+    if (num % 2 == 0) {
+        num + 1;
+    } else {
+        num + 1;
+    }
 
-
-
+    console.log(num);
 
 }
---
+++ exer_06.js
@@ -0,0 +1,16 @@
+function verificador() {
+
+let letra = []
+let a = [a]
+let e = [e]
+let i = [i]
+let o = [o]
+let u = [u]
+
+if(letra == a, e, i, o, u) {
+    console.log("Vogal");
+}else{
+    console.log("Consoante")
+}
+
+}
